f3a7821b0974133d4c43c3300e1ddcc47c639b61
miali88 <m.ali@gacm.co.uk>
2024-08-08 21:11:12 +0700

    Initial commit of project

--- backend/app/backend_pre_start.py
+++ backend/app/backend_pre_start.py
@@ -4,6 +4,7 @@ from sqlalchemy import Engine
 from sqlmodel import Session, select
 from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
 
+#secret
 from app.core.db import engine
 
 logging.basicConfig(level=logging.INFO)
